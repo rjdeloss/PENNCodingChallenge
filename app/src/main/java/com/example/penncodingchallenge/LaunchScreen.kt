54b6a199f98d10788cc34bf95a5a40f8aced514f
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-09-24 12:16:36 -0400

    create LaunchScreen

---
+++ app/src/main/java/com/example/penncodingchallenge/LaunchScreen.kt
@@ -0,0 +1,46 @@
+package com.example.penncodingchallenge
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LaunchScreen() {
+    val modifier = Modifier
+
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.primary
+    ) {
+        Column(
+            modifier= modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.penn_logo),
+                    contentDescription = "Penn Logo",
+                    modifier.size(200.dp)
+                )
+                Spacer(modifier = modifier.height(16.dp))
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun LaunchScreenPreview() {
+    LaunchScreen()
+}

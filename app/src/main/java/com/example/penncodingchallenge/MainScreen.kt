319df99bbae99e4b41c6afd035b41f443bd891bb
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-09-25 14:44:35 -0400

    create skeleton of main screen

--- app/src/main/java/com/example/penncodingchallenge/MainScreen.kt
+++ app/src/main/java/com/example/penncodingchallenge/MainScreen.kt
@@ -1,9 +1,13 @@
 package com.example.penncodingchallenge
 
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -12,13 +16,22 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    SearchBar(modifier.padding(16.dp))
+    Column(verticalArrangement = Arrangement.SpaceBetween) {
+        SearchBar(modifier.padding(16.dp))
+        LocationInfoCard(modifier.padding(16.dp))
+        AQITabs(modifier.padding(16.dp))
+        ForecastCard(modifier.padding(16.dp))
+    }
+
 }
 
 @Composable
@@ -43,8 +56,51 @@ fun SearchBar(
             .fillMaxWidth()
             .heightIn(min = 56.dp)
     )
-
 }
+
+@Composable
+fun LocationInfoCard(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Location: Longitude, Latitude")
+        Text(text = "City: Bangkok")
+        Text(text = "Weather Station: AG1")
+    }
+}
+
+@Composable
+fun AQITabs(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "AQI Information", fontSize = 28.sp )
+        Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceAround) {
+            Column (horizontalAlignment = Alignment.CenterHorizontally){
+                Text(text = "Yesterday", fontSize = 20.sp)
+                Text(text = "32", fontSize = 48.sp, fontWeight = FontWeight.Bold)
+                Text(text = "Good", fontSize = 20.sp)
+            }
+            Column (horizontalAlignment = Alignment.CenterHorizontally){
+                Text(text = "Today", fontSize = 20.sp)
+                Text(text = "69", fontSize = 48.sp, fontWeight = FontWeight.Bold)
+                Text(text = "Moderate", fontSize = 20.sp)
+            }
+            Column (horizontalAlignment = Alignment.CenterHorizontally){
+                Text(text = "Tomorrow", fontSize = 20.sp)
+                Text(text = "129", fontSize = 48.sp, fontWeight = FontWeight.Bold)
+                Text(text = "Unhealthy", fontSize = 20.sp)
+            }
+        }
+    }
+}
+
+@Composable
+fun ForecastCard(modifier: Modifier =Modifier) {
+    Column(modifier = modifier) {
+        Text(text = "Forecast", fontSize = 28.sp)
+        Text(text = "average: 154", fontSize = 24.sp)
+        Text(text = "min: 131", fontSize = 24.sp)
+        Text(text = "max: 157", fontSize = 24.sp)
+    }
+}
+
 @Preview
 @Composable
 private fun MainScreenPreview() {

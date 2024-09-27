0e315dc0d32e93f82c3542ea4425c2221a211abe
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-09-27 08:29:12 -0400

    WIP implementing API call to test response

--- app/src/main/java/com/example/penncodingchallenge/HomeViewModel.kt
+++ app/src/main/java/com/example/penncodingchallenge/HomeViewModel.kt
@@ -1,15 +1,52 @@
 package com.example.penncodingchallenge
 
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.penncodingchallenge.models.AqiData
+import com.example.penncodingchallenge.models.LocationState
+import com.example.penncodingchallenge.utils.RetrofitInstance
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
 
-class MainViewModel: ViewModel() {
-    private val uiState = MutableStateFlow(MainState())
-    val state: StateFlow<MainState> = uiState;
+class HomeViewModel: ViewModel() {
+    private val uiState = MutableStateFlow(LocationState())
+    val state: StateFlow<LocationState> = uiState;
 
-    fun update(latitude: Double, longitude: Double) {
+    private val _aqiData = MutableStateFlow(AqiData())
+    val aqiData : StateFlow<AqiData> = _aqiData
+
+    fun updateLocation(latitude: Double, longitude: Double) {
         uiState.update { it.copy(latitude, longitude) }
+    }
+
+    private fun retrieveAqiByCity() {
+        viewModelScope.launch {
+            val call : Call<AqiData> =
+                RetrofitInstance.api.getAqiWithCity("newyork", "dae702f98ec0f3a68a36b6b49b327b4c781727e9")
+            call.enqueue(object : Callback<AqiData> {
+                override fun onResponse(
+                    call: Call<AqiData>,
+                    response: Response<AqiData>
+                ) {
+                    if(response.isSuccessful) {
+                        val responseData: AqiData? = response.body()
+                        if (responseData != null) {
+                            _aqiData.value = responseData
+                        }
+                    }
+                }
+                override fun onFailure(call: Call<AqiData>, response: Throwable) {
+                    Log.d("Failed to retrieve data", "Network Error")
+                }
+            })
+        }
     }
 }
--- app/src/main/java/com/example/penncodingchallenge/MainActivity.kt
+++ app/src/main/java/com/example/penncodingchallenge/MainActivity.kt
@@ -4,20 +4,33 @@ import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.Looper
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.penncodingchallenge.models.AqiData
+import com.example.penncodingchallenge.screens.LaunchScreen
+import com.example.penncodingchallenge.screens.HomeScreen
+import com.example.penncodingchallenge.utils.RetrofitInstance
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import okio.IOException
+import retrofit2.HttpException
 
 class MainActivity : ComponentActivity() {
     private val LOCATION_PERMISSION_REQUEST_CODE = 1
@@ -51,12 +64,12 @@ class MainActivity : ComponentActivity() {
     private lateinit var locationClient: FusedLocationProviderClient
 
     @SuppressLint("MissingPermission")
-    private fun initUpdates(viewModel: MainViewModel) {
+    private fun initUpdates(viewModel: HomeViewModel) {
         locationClient = LocationServices.getFusedLocationProviderClient(this)
 
         locationClient.requestLocationUpdates(
             createLocationRequest(),
-            { location -> viewModel.update(location.latitude, location.longitude)},
+            { location -> viewModel.updateLocation(location.latitude, location.longitude)},
             Looper.getMainLooper()
         )
     }
@@ -70,14 +83,21 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         checkPermissions()
-        val viewModel = MainViewModel()
+        val viewModel = HomeViewModel()
         val modifier: Modifier = Modifier
 
         initUpdates(viewModel)
 
         setContent {
+            val context = LocalContext.current
+            var airQualityData by remember {
+                mutableStateOf(AqiData())
+            }
+
             val navController = rememberNavController()
             val scope = rememberCoroutineScope()
+
+
 
             NavHost(navController = navController, startDestination = "launch_screen") {
                 composable("launch_screen") {
@@ -92,7 +112,7 @@ class MainActivity : ComponentActivity() {
                     }
                 }
                 composable("main_screen") {
-                    MainScreen(modifier, viewModel)
+                    HomeScreen(modifier, viewModel)
                 }
             }
 

4a85b61080791db4b24ea7a0b9015317f15c8928
Rafiluis De Los Santos <rjdeloss.dev@gmail.com>
2024-10-08 00:56:35 -0400

    implement okhttpclient to add token as parameter in one location

--- app/src/main/java/com/example/penncodingchallenge/utils/RetrofitInstance.kt
+++ app/src/main/java/com/example/penncodingchallenge/utils/RetrofitInstance.kt
@@ -1,19 +1,37 @@
 package com.example.penncodingchallenge.utils
 
-import com.example.penncodingchallenge.AqiApiInterface
+import com.example.penncodingchallenge.AqiApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import okhttp3.OkHttpClient as OkHttpClient
 
 object RetrofitInstance {
     private const val BASE_URL = "https://api.waqi.info"
 
-    val api : AqiApiInterface by lazy {
-        Retrofit
-            .Builder()
+    val api : AqiApiService by lazy {
+        Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(
+                OkHttpClient.Builder()
+                    .addInterceptor { chain ->
+                        val urlWithApiKey = chain.request().url.newBuilder()
+                            .addQueryParameter("token", "dae702f98ec0f3a68a36b6b49b327b4c781727e9")
+                            .build()
+                        val request = chain.request().newBuilder()
+                            .url(urlWithApiKey)
+                            .build()
+                        chain.proceed(request)
+                    }
+                    .addInterceptor(
+                        HttpLoggingInterceptor().apply {
+                            level = HttpLoggingInterceptor.Level.BODY
+                        }
+                    )
+                    .build()
+            )
             .build()
-            .create(AqiApiInterface::class.java)
+            .create(AqiApiService::class.java)
     }
 }
